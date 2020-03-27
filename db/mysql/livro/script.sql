5f0b5034ae7e2ef14157e777921ec77d3a9ad2bf
Alex Marquezini <alexmarquezini@gmail.com>
2020-03-26 22:43:15 -0300

    Banco de dados 26-03-2020

---
+++ db/mysql/livro/script.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE `comercial` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE `comercial`;
+
+
+CREATE TABLE IF NOT EXISTS `comclien` (
+  `n_numeclien` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `c_codiclien` VARCHAR(10),
+  `c_nomeclien` VARCHAR(100),
+  `c_razaclien` VARCHAR(100),
+  `d_dataclien` DATE,
+  `c_cnpjclien` VARCHAR(20),
+  `c_foneclien` VARCHAR(20),
+  PRIMARY KEY `pk_id`(`n_numeclien`)
+) ENGINE = InnoDB;
+
+/*DESC comclien;*/
+
